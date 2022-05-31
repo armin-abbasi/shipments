3e772abe1d54a48cab6d60ab79c6127820791738
Armin Abbasi <armin.abbasi@hotmail.com>
2022-05-31 21:08:29 +0430

    Created migrations

---
+++ project/database/migrations/2022_05_30_193437_create_shipments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShipmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('distance');
+            $table->smallInteger('time');
+            $table->integer('price')->nullable();
+            $table->unsignedBigInteger('company_id');
+            $table->unsignedBigInteger('carrier_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipments');
+    }
+}
