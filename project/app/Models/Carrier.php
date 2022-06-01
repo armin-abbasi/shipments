fd0d10bb1a796f9b84924ff374d91c223e73ee3a
Armin Abbasi <armin.abbasi@hotmail.com>
2022-06-01 21:14:11 +0430

    Created remaining models

---
+++ project/app/Models/Carrier.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Carrier extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+    ];
+
+    public function shipments()
+    {
+        return $this->hasMany(Shipment::class);
+    }
+}
---
+++ project/app/Models/Route.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Route extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'stop_id',
+        'postcode',
+        'city',
+        'country',
+        'type',
+    ];
+
+    public function shipments()
+    {
+        return $this->belongsToMany(Shipment::class);
+    }
+}
