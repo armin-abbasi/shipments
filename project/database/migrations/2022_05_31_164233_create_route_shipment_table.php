d807f3120a82ae808baf0deceaf91294bf883d6b
Armin Abbasi <armin.abbasi@hotmail.com>
2022-06-01 21:13:51 +0430

    Created route shipment migrations

---
+++ project/database/migrations/2022_05_31_164233_create_route_shipment_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRouteShipmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('route_shipment', function (Blueprint $table) {
+            $table->unsignedBigInteger('route_id');
+            $table->foreign('route_id')
+                ->references('id')
+                ->on('routes')
+                ->onDelete('cascade');
+            $table->unsignedBigInteger('shipment_id');
+            $table->foreign('shipment_id')
+                ->references('id')
+                ->on('shipments')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('route_shipment');
+    }
+}
