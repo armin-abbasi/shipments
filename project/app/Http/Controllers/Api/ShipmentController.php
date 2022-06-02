589bfc811c4f640e02d4e76da749c13321c32952
Armin Abbasi <armin.abbasi@hotmail.com>
2022-06-03 00:55:36 +0430

    Implemented all of the api routes

--- project/app/Http/Controllers/Api/ShipmentController.php
+++ project/app/Http/Controllers/Api/ShipmentController.php
@@ -4,15 +4,83 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Shipments\GetAllRequest;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
 use App\Repositories\ShipmentRepository;
 use Illuminate\Http\Response;
 
 class ShipmentController extends Controller
 {
-    public function index(GetAllRequest $request, ShipmentRepository $shipmentRepository)
+    protected $repo;
+
+    /**
+     * ShipmentController constructor.
+     * @param ShipmentRepository $shipmentRepository
+     */
+    public function __construct(ShipmentRepository $shipmentRepository)
     {
-        $data = $shipmentRepository->getAll($request->all());
+        $this->repo = $shipmentRepository;
+    }
+
+    /**
+     * @param GetAllRequest $request
+     * @return ResponseFactory|Response
+     */
+    public function index(GetAllRequest $request)
+    {
+        $data = $this->repo->getAll($request->all());
+
+        return response($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @param int $shipmentID
+     * @return ResponseFactory|Response
+     */
+    public function getOneShipment(int $shipmentID)
+    {
+        $data = $this->repo->getOneShipment($shipmentID);
+
+        return response($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @param int $carrierID
+     * @param Request $request
+     * @return ResponseFactory|Response
+     */
+    public function carrierShipments(int $carrierID, Request $request)
+    {
+        $data = $this->repo->getCarrierShipments(array_merge([
+            'carrier_id' => $carrierID,
+            $request->all(),
+        ]));
+
+        return response($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @param int $companyID
+     * @param Request $request
+     * @return ResponseFactory|Response
+     */
+    public function companyShipments(int $companyID, Request $request)
+    {
+        $data = $this->repo->getCompanyShipments(array_merge([
+            'company_id' => $companyID,
+            $request->all(),
+        ]));
+
+        return response($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @param int $routeID
+     * @return ResponseFactory|Response
+     */
+    public function routeShipments(int $routeID)
+    {
+        $data = $this->repo->getRouteShipments($routeID);
 
         return response($data, Response::HTTP_OK);
     }
--- project/routes/api.php
+++ project/routes/api.php
@@ -20,5 +20,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function () {
-    Route::get('get', [ShipmentController::class, 'index']);
+    Route::get('shipments', [ShipmentController::class, 'index']);
+    Route::get('shipments/{shipment_id}', [ShipmentController::class, 'getOneShipment']);
+    Route::get('companies/{company_id}/shipments', [ShipmentController::class, 'companyShipments']);
+    Route::get('carriers/{carrier_id}/shipments', [ShipmentController::class, 'carrierShipments']);
+    Route::get('routes/{route_id}/shipments', [ShipmentController::class, 'routeShipments']);
 });
